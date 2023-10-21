2427c0c54824d3b5613b40c1c32838300726360f
David Porter <david@daporter.net>
2023-10-21 18:18:48 +0200

    Add my personal configuration

--- config.def.h
+++ config.def.h
@@ -6,19 +6,19 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Iosevka Nerd Font Propo:pixelsize=11:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#d4be98", "#282828" },
+	[SchemeSel] = { "#282828", "#d8a657" },
+	[SchemeOut] = { "#282828", "#a9b665" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 0;
+static unsigned int lineheight = 20;
 static unsigned int min_lineheight = 8;
 
 /*
